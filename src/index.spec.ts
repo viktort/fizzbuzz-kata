562ad65b695841594920d3f1e24f668e5bc0f929
Lau Llobet <lau.llobet@mypulse.ai>
2021-02-02 15:45:30 +0100

    🔴🔴🔴🔴 RED - shall return a string - to squash

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -6,6 +6,6 @@ export function sum(a: number, b: number): number {
 
 describe('sample test-suite', () => {
     it('sum 1 + 3 should be 4', () => {
-        expect(sum(1, 3)).eql(4);
+        expect(sum(1, 3)).eql("4");
     });
 });
