ecc5ab7b7a9d348919a4fb5688d1018a0917f975
Ruth Wall <ruth.wall@ruth.wall.macbook>
2021-02-09 16:36:49 +0000

    skeleton fizzbuzz, wrong assumption on case 15

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,34 @@
 import { expect } from 'chai';
 
-function sum(a: number, b: number): number {
-    return a + b;
+function fizzbuzz(a: number): string {
+
+    if (!(a % 5)) {
+        return "Buzz";
+    }
+
+    if (!(a % 3)) {
+        return "Fizz";
+    }
+
+    return a.toString();
 }
 
-describe('sample test-suite', () => {
-    it('sum 1 + 3 should be 4', () => {
-        expect(sum(1, 3)).eql(4);
-    });
+describe("Test fizzbuzz", () => {
+    const ourNumbers = {
+        1: "1",
+        2: "2",
+        4: "4",
+        3: "Fizz",
+        6: "Fizz",
+        9: "Fizz",
+        12: "Fizz",
+        5: "Buzz",
+        10: "Buzz",
+        15: "Buzz"
+    };
+    for (const num in ourNumbers) {
+        it("input number to return expected response", () => {
+            expect(fizzbuzz(parseInt(num))).eql(ourNumbers[num]);
+        });
+    }
 });
