bc8653077ca491065d7b6ce277ab8508c6feb67c
Ruth Wall <ruth.wall@ruth.wall.macbook>
2021-02-09 16:44:56 +0000

    mob next [ci-skip]

--- src/leapYear.spec.ts
+++ src/leapYear.spec.ts
@@ -1,7 +1,14 @@
 import { expect } from 'chai';
 
+function leapCalculate(year: number) {
+    return true;
+}
+
 describe("Leap Year Calculator", () => {
     it("returns true if its 1996", () => {
-        expect(leapCalculate(1996)).toEql(true);
+        expect(leapCalculate(1996)).eql(true);
+    });
+    it("returns false if its 2001", () => {
+        expect(leapCalculate(2001)).eql(false);
     });
 });
